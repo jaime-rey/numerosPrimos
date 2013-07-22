4ce2d1d7033176b3d5f5cc385798f788cdca2f9b
jaime-rey <jaimereycasado@gmail.com>
2013-07-23 00:35:55 +0200

    finalizado

--- Numeros Primos/src/com/monokepos/view/NumerosPrimos.java
+++ Numeros Primos/src/com/monokepos/view/NumerosPrimos.java
@@ -1,13 +1,16 @@
 package com.monokepos.view;
 
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
-import javax.swing.JTextPane;
 
 public class NumerosPrimos extends JFrame implements Runnable, ActionListener {
 
@@ -15,8 +18,11 @@ public class NumerosPrimos extends JFrame implements Runnable, ActionListener {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private JTextField textField;
+	private JTextField textField = new JTextField("250", 10);
 	private Thread ir;
+	JButton btnMostrarPrimos = new JButton("Mostrar Primos");
+	JLabel lblNewLabel = new JLabel("Cantidad");
+	JTextArea textArea = new JTextArea(8, 40);
 
 	/**
 	 * Create the panel.
@@ -24,43 +30,83 @@ public class NumerosPrimos extends JFrame implements Runnable, ActionListener {
 	public NumerosPrimos() {
 		super();
 		setSize(400, 200);
+		setTitle("Primos");
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		BorderLayout borde = new BorderLayout();
+		setLayout(borde);
 
-		setLayout(null);
-
-		JLabel lblNewLabel = new JLabel("Cantidad");
 		lblNewLabel.setBounds(28, 25, 78, 14);
-		add(lblNewLabel);
 
-		textField = new JTextField("250", 10);
-		textField.setBounds(114, 22, 86, 20);
-		add(textField);
-		textField.setColumns(10);
-
-		JButton btnMostrarPrimos = new JButton("Mostrar Primos");
 		btnMostrarPrimos.setBounds(241, 22, 154, 21);
-		add(btnMostrarPrimos);
 
-		JTextPane textPane = new JTextPane();
-		textPane.setBounds(10, 63, 430, 226);
-		add(textPane);
+		btnMostrarPrimos.addActionListener(this);
 
+		JPanel topPanel = new JPanel();
+		topPanel.add(lblNewLabel);
+		topPanel.add(textField);
+		topPanel.add(btnMostrarPrimos);
+		add(topPanel, BorderLayout.NORTH);
+
+		textArea.setLineWrap(true);
+		JScrollPane panelTexto = new JScrollPane(textArea);
+		add(panelTexto, BorderLayout.CENTER);
+
+		setVisible(true);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
 
+		btnMostrarPrimos.setEnabled(false);
+		if (ir == null) {
+			ir = new Thread(this);
+			ir.start();
+		}
+		// int cantidad = Integer.parseInt(textField.getText());
+		// int numPrimos = 0;
+		// int candidato = 2;
+		// textArea.append("Primeros " + cantidad + " numeros primos: ");
+		// while (numPrimos < cantidad) {
+		// if (esPrimo(candidato)) {
+		// textArea.append(candidato + " ");
+		// numPrimos++;
+		// }
+		// candidato++;
+		// }
 	}
 
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
+		int cantidad = Integer.parseInt(textField.getText());
+		int numPrimos = 0;
+		int candidato = 2;
+		textArea.append("Primeros " + cantidad + " numeros primos: ");
+		while (numPrimos < cantidad) {
+			if (esPrimo(candidato)) {
+				textArea.append(candidato + " ");
+				numPrimos++;
+			}
+			candidato++;
+		}
 
+	}
+
+	private static boolean esPrimo(int candidato) {
+		// TODO Auto-generated method stub
+		double raiz = Math.sqrt(candidato);
+		for (int i = 2; i <= raiz; i++) {
+			if (candidato % i == 0) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	public static void main(String[] args) {
 		NumerosPrimos n = new NumerosPrimos();
-		n.setVisible(true);
+		System.out.println(n.toString());
 	}
 }
