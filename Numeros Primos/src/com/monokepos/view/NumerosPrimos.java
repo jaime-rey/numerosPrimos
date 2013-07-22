e191ec50d684ee9e5095724c74da11aaca94f406
jaime-rey <jaimereycasado@gmail.com>
2013-07-22 22:00:24 +0200

    segundo commit

--- Numeros Primos/src/com/monokepos/view/NumerosPrimos.java
+++ Numeros Primos/src/com/monokepos/view/NumerosPrimos.java
@@ -1,41 +1,58 @@
 package com.monokepos.view;
 
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 import javax.swing.JTextPane;
 
-public class NumerosPrimos extends JPanel {
+public class NumerosPrimos extends JFrame implements Runnable, ActionListener {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	private JTextField textField;
+	private Thread ir;
 
 	/**
 	 * Create the panel.
 	 */
 	public NumerosPrimos() {
+
 		setLayout(null);
 
 		JLabel lblNewLabel = new JLabel("Cantidad");
 		lblNewLabel.setBounds(28, 25, 78, 14);
 		add(lblNewLabel);
-		
+
 		textField = new JTextField();
 		textField.setBounds(114, 22, 86, 20);
 		add(textField);
 		textField.setColumns(10);
-		
+
 		JButton btnMostrarPrimos = new JButton("Mostrar Primos");
 		btnMostrarPrimos.setBounds(241, 22, 154, 21);
 		add(btnMostrarPrimos);
-		
+
 		JTextPane textPane = new JTextPane();
 		textPane.setBounds(10, 63, 430, 226);
 		add(textPane);
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
 
 	}
 }
